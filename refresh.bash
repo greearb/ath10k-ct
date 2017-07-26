3ed4af4ec2ad8f77985f9bae61129f33a3edd390
Ben Greear <greearb@candelatech.com>
2017-07-26 13:43:00 -0700

    refresh:  Properly copy the 4.4 files into place.

--- refresh.bash
+++ refresh.bash
@@ -7,5 +7,5 @@ cp -ar ~/git/linux-$KVER.dev.y/drivers/net/wireless/ath/ath10k ./
 
 # And for 4.4 kernel
 KVER=4.4
-cp -ar ~/git/linux-$KVER.dev.y/drivers/net/wireless/ath/ath10k ./ath10k-4.4
+cp -ar ~/git/linux-$KVER.dev.y/drivers/net/wireless/ath/ath10k/* ./ath10k-4.4
 
