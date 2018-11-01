6c3a6262317f3240013014e79a570693009c0235
Ben Greear <greearb@candelatech.com>
2018-11-01 12:31:46 -0700

    ath10k-ct:  Import initial 4.19 kernel port attempt.
    This code is very fresh, just rebased all my patches and
    compile tested.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- refresh.bash
+++ refresh.bash
@@ -22,4 +22,9 @@ KVER=4.16
 mkdir -p ./ath10k-$KVER
 cp -ar ~/git/linux-$KVER.dev.y/drivers/net/wireless/ath/ath10k/* ./ath10k-$KVER
 
+# And for 4.19 kernel
+KVER=4.19
+mkdir -p ./ath10k-$KVER
+cp -ar ~/git/linux-$KVER.dev.y/drivers/net/wireless/ath/ath10k/* ./ath10k-$KVER
+
 
