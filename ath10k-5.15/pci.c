e8e166fee23226e5d495afbdb7ed681d747d115b
Ben Greear <greearb@candelatech.com>
2021-11-21 08:54:00 -0800

    ath10k-ct:  Fix kernel version string.
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- ath10k-5.15/pci.c
+++ ath10k-5.15/pci.c
@@ -3874,7 +3874,7 @@ static int __ath10k_pci_probe(struct pci_dev *pdev,
 	int (*pci_hard_reset)(struct ath10k *ar);
 	u32 (*targ_cpu_to_ce_addr)(struct ath10k *ar, u32 addr);
 
-	printk(KERN_INFO "ath10k 5.12 driver, optimized for CT firmware, probing pci device: 0x%x.\n",
+	printk(KERN_INFO "ath10k 5.15 driver, optimized for CT firmware, probing pci device: 0x%x.\n",
 	       pci_dev->device);
 
 	switch (pci_dev->device) {
